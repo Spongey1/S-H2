0da4127f84288e463d171cd1b00147598b62253d
Spongey1 <Driton.latifi101@gmail.com>
2022-05-12 09:06:11 +0200

    isHTML => isText

--- MessageManager.cs
+++ MessageManager.cs
@@ -3,20 +3,20 @@ namespace S_H2
     // Handles the sending of messages
     public class MessageManager
     {
-        public void sendMessage(MessageCarrier type, string[] to, Message m, bool isHTML)
+        public void sendMessage(MessageCarrier type, string[] to, Message m, bool isText)
         {
             ConvertText Convertion = new ConvertText();
             //herinde sendes der en email ud til modtageren
             if (type.Equals(MessageCarrier.Smtp))
             {
-                if (isHTML)
+                if (isText)
                     m.Body = Convertion.ConvertBodyToText(m.Body);
                 //her implementeres alt koden til at sende via Smtp
             }
 
             if (type.Equals(MessageCarrier.VMessage))
             {
-                if (isHTML)
+                if (isText)
                     m.Body = Convertion.ConvertBodyToText(m.Body);
                 //her implementeres alt koden til at sende via VMessage
             }
